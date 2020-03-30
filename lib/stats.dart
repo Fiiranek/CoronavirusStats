d48f34fa6a6ef03de03b90b3d4c35669305260b4
Fiiranek <jellybean@onet.eu>
2020-03-30 11:57:54 +0200

    3 commit

--- lib/stats.dart
+++ lib/stats.dart
@@ -51,10 +51,6 @@ class _StatsState extends State<Stats> {
   Future<TotalStats> getData() async {
     final response =
         await http.get('https://api.apify.com/v2/key-value-stores/3Po6TV7wTht4vIEid/records/LATEST?disableRedirect=true');
-  //       await http.get('https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Poland',headers: {
-	// 	"x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-	// 	"x-rapidapi-key": "3365d0197amsha3584a952c169f8p1094d0jsn85d8b8022cd8"
-	// });
     return TotalStats.fromJson(json.decode(response.body));
   }
 
@@ -73,69 +69,10 @@ class _StatsState extends State<Stats> {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
     new GlobalKey<RefreshIndicatorState>();
 
-  TextStyle columnText = TextStyle(fontSize: 14,fontWeight: FontWeight.w500);
+  TextStyle columnText = TextStyle(fontWeight: FontWeight.w500);
 
   @override
   Widget build(BuildContext context) {
-
-    showAlertDialog(BuildContext context,region,infectedCount,deceasedCount) {
-
-  // set up the button
-  Widget okButton = FlatButton(
-    child: Text("OK"),
-    onPressed: () { 
-      Navigator.pop(context);
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    
-    titlePadding: EdgeInsets.zero,
-    contentPadding: EdgeInsets.all(10),
-    title: Container(
-      decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Color(0xff2B5E80),
-                    Color(0xff5BAFE7),
-                  ])
-                ),
-      child: Padding(
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: <Widget>[
-          Image.asset('assets/virus.png',width: 35,),
-                  SizedBox(width: 5,),
-          Text("Województwo "+region,style: TextStyle(color: Colors.white),),
-        ],
-      ),
-    )),
-    content: 
-        Container(
-          height: 40,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text("Zarażenia: "+infectedCount.toString()),
-              Text("Zgony: "+deceasedCount.toString()),
-            ],
-          ),
-        ),
-      
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
 
     return FutureBuilder(
       future: futureData,
@@ -156,9 +93,6 @@ class _StatsState extends State<Stats> {
                 Text(data[i].region[0].toUpperCase()+data[i].region.substring(1),style: TextStyle(fontWeight: FontWeight.w600),),
                 ),
               DataCell(Text(data[i].infectedCount.toString(),),
-              onTap: (){
-                showAlertDialog(context,data[i].region[0].toUpperCase()+data[i].region.substring(1),data[i].infectedCount,data[i].deceasedCount);
-              }
               ),
               DataCell(Text(data[i].deceasedCount.toString(),)),
             ]));
